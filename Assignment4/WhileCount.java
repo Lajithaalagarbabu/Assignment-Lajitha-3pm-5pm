671334d4d714ae1878901236e11c6d9a5b817ed5
Lajithaalagarbabu <166518603+Lajithaalagarbabu@users.noreply.github.com>
2026-03-05 18:39:42 +0530

    Assingnmet-4

---
+++ Assignment4/WhileCount.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class WhileCount{
+public static void main(String []args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a input:");
+int num=sc.nextInt();
+int count=0;
+while(num!=0){  
+    num= num/10;
+     count ++; 
+}
+System.out.println(count);
+}
+
+}
